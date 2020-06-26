8cad2fd02c164eb10f0debcc05a0bb9284f541f9
SaiTejaReddyYeldandi <60148881+SaiTejaReddyYeldandi@users.noreply.github.com>
2020-06-26 23:07:03 +0530

    Add files via upload

---
+++ DesignPattern/src/main/java/epam/DesignPattern/Observer.java
@@ -0,0 +1,32 @@
+
+package epam.DesignPattern;
+import java.util.*;
+public class Observer{
+        static class SachinCenturyNotifier {
+            List<SachinFan> fans = new ArrayList<SachinFan>();
+            void register(SachinFan fan) {
+                fans.add(fan);
+            } 
+            void sachinScoredACentury() {
+                for(SachinFan fan: fans) {
+                    fan.announce();
+                }
+            }
+        }
+        static class SachinFan {
+            private String name;
+            SachinFan(String name) {
+                this.name = name;
+            }
+            void announce() {
+                System.out.println(name + " notified");
+            }
+        }
+        public static void main(String[] args) {
+            SachinCenturyNotifier notifier = new SachinCenturyNotifier();
+            notifier.register(new SachinFan("Ranga"));
+            notifier.register(new SachinFan("Ramya"));
+            notifier.register(new SachinFan("Veena"));
+            notifier.sachinScoredACentury();
+        }
+    }
---
+++ DesignPattern/src/main/java/epam/DesignPattern/SingletonPattern.java
@@ -0,0 +1,12 @@
+package epam.DesignPattern;
+
+public class SingletonPattern {
+	private static SingletonPattern obj;
+	public static SingletonPattern getInstance() {
+		if(obj == null){
+			obj = new SingletonPattern();
+		}
+		return obj;
+	}
+	
+}
